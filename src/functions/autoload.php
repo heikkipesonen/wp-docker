cf8a2aa7127c586cb3fd525e68f3c571cf7e8553
Heikki Pesonen <pesonen.heikki@gmail.com>
2017-03-21 15:32:07 +0200

    move autoloader

---
+++ src/functions/autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+function toCamel($string, $capitalizeFirstCharacter = false) {
+    $str = str_replace('-', '', ucwords($string, '-'));
+    if (!$capitalizeFirstCharacter) {
+        $str = lcfirst($str);
+    }
+    return $str;
+}
+
+function toKebab($input) {
+  preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
+  $ret = $matches[0];
+  foreach ($ret as &$match) {
+    $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+  }
+  return implode('_', $ret);
+}
+
+spl_autoload_register(function ($name) {
+  include 'components/'.strtolower(toKebab($name)).'.php';
+});
--- src/header.php
+++ src/header.php
@@ -1,25 +1,4 @@
 <?php
-function toCamel($string, $capitalizeFirstCharacter = false) {
-    $str = str_replace('-', '', ucwords($string, '-'));
-    if (!$capitalizeFirstCharacter) {
-        $str = lcfirst($str);
-    }
-    return $str;
-}
-
-function toKebab($input) {
-  preg_match_all('!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!', $input, $matches);
-  $ret = $matches[0];
-  foreach ($ret as &$match) {
-    $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
-  }
-  return implode('_', $ret);
-}
-
-spl_autoload_register(function ($name) {
-  include 'components/'.strtolower(toKebab($name)).'.php';
-});
-
 // $frontpage = get_post( get_option( 'page_on_front' ) );
 // $fields = get_fields($frontpage->ID);
 //
